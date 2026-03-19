f8d82462b9eb89acce4e859722cd1a3815ec5fcc
Marcin Suder <marcin.code.github@gmail.com>
2026-03-19 23:03:44 +0100

    add code

--- server.mjs
+++ server.mjs
@@ -3,23 +3,23 @@ import path from 'path';
 
 const server = jsonServer.create();
 const router = jsonServer.router('build/db/app.json');
-const middlewares = jsonServer.defaults({ static: 'build', noCors: true });
 
-const port = process.env.PORT || 3000;
+const middlewares = jsonServer.defaults({
+  static: 'build',
+  noCors: true
+});
+server.use(middlewares);
 
 server.use(jsonServer.rewriter({ '/api/*': '/$1' }));
 
-server.use(middlewares);
+server.use(router);
 
 server.get('*', (req, res, next) => {
-  if (req.path.startsWith('/api') || req.path.includes('.')) {
-    return next();
-  }
+  if (req.path.startsWith('/api') || req.path.includes('.')) return next();
   res.sendFile(path.resolve('build/index.html'));
 });
 
-server.use(router);
-
+const port = process.env.PORT || 3000;
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
